ac65ed47d3dc8921ac2505fbbaa2906d6246de6d
Alexander Yasko <a.yasko@pm.me>
2023-12-25 11:44:25 +0100

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     snapshot: true
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Messaging Demo')
+    .setDescription('Employee API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
